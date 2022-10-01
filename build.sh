5273c2b8954da89c263ec8445e4e8fda63a9908f
apelsimon <s.eriksson89@gmail.com>
2022-10-01 19:25:17 +0200

    init commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT="$(git rev-parse --show-toplevel)"
+OUTPUT_DIR="${ROOT}/build"
+PUBLIC_DIR="${OUTPUT_DIR}/public"
+
+rm -rf ${OUTPUT_DIR}
+
+mkdir -p "${PUBLIC_DIR}"
+
+cd client
+npm run build
+mv dist/* "${PUBLIC_DIR}"
+
+cd ${ROOT}/server
+cargo build
+cp target/debug/server ${OUTPUT_DIR}
---
+++ client/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>About to bring you a dodoidoidoi soon...</h1>
+  </div>
+</template>
